2be96a9dc4393d182920c4275492faa2796583b9
kqueryful <kqueryful@gmail.com>
2016-10-12 00:42:50 -0700

    Updated scrapeArticles.py
    Updated for python 3

--- scrapeArticles.py
+++ scrapeArticles.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 
 import re
 import csv
 import json
 import glob
-import urllib
+from urllib import request
 import os.path
 import sqlite3
 
@@ -16,26 +16,26 @@ def downloadFiles():
 
     # download news-list
 	url = "http://www3.nhk.or.jp/news/easy"
-	urllib.urlretrieve("%s/news-list.json" % (url), "news/news-list.json")
+	request.urlretrieve("%s/news-list.json" % (url), "news/news-list.json")
 
 	# parse news-list
-	data = json.loads(open("news/news-list.json").readline()[3:])
+	data = json.loads(open("news/news-list.json", encoding="latin-1").readline()[3:])
 
 	for day in data[0]:
 		print (day)
 		for index, val in enumerate(data[0][day]):
 			id = data[0][day][index]["news_id"]
-            
+
 			# if new, download dictionary, json files
 			dname = "news/%s.out.dic" % (id)
 			jname = "news/%s.out.json" % (id)
 			if not os.path.isfile(dname):
-				urllib.urlretrieve("%s/%s/%s.out.dic" % (url, id, id), dname)
-				
+				request.urlretrieve("%s/%s/%s.out.dic" % (url, id, id), dname)
+
 			if not os.path.isfile(jname):
 				print ("downloading %s" % id)
-				urllib.urlretrieve("%s/%s/%s.out.json" % (url, id, id), jname)
-	
+				request.urlretrieve("%s/%s/%s.out.json" % (url, id, id), jname)
+
 def tableSetup():
 	try:
 		# clear old tables
@@ -44,7 +44,7 @@ def tableSetup():
 		c.execute("DROP TABLE Examples")
 	except sqlite3.OperationalError:
 		pass
-	try: 
+	try:
 		# create new tables
 		c.execute("CREATE TABLE Articles (newsid TEXT PRIMARY KEY, text TEXT)")
 		c.execute("CREATE TABLE Words (word TEXT PRIMARY KEY, reading TEXT, frequency INTEGER, category TEXT, definition TEXT, alt TEXT, wiki TEXT, jdic TEXT)")
@@ -53,24 +53,24 @@ def tableSetup():
 		pass
 
 	conn.commit()
-	
+
 if __name__ == '__main__':
 	downloadFiles()
-	
+
 	# SQLite setup
 	conn = sqlite3.connect('newsWeb.sqlite')
 	c = conn.cursor()
 	tableSetup()
-	
+
 	# for each article
 	for files in glob.glob("news/*.out.json"):
-		article = json.load(open(files))
-		
+		article = json.load(open(files, encoding="latin-1"))
+
 		# parse text into sentences
 		articleText = article["text"]
 		c.execute("INSERT OR IGNORE INTO Articles (newsid, text) VALUES (?,?)", (article["newsid"], articleText))
 		articleLines = articleText.split(u"。")
-		
+
 		# for each word
 		for item in article["morph"]:
 			# proper nouns
@@ -80,7 +80,7 @@ if __name__ == '__main__':
 				for index, val in enumerate(item["ruby"]):
 					try: reading += item["ruby"][index]["r"]
 					except KeyError: reading += item["ruby"][index]["s"]
-				
+
 				# insert into sql
 				try:
 					c.execute("INSERT INTO Words (word, reading, frequency, category) VALUES (?,?,?,?)", \
@@ -93,7 +93,7 @@ if __name__ == '__main__':
 					if item["word"] in line:
 						sentence = re.sub(item["word"], "<b>"+item["word"]+"</b>", line.strip()+u"。")
 						c.execute("INSERT OR IGNORE INTO Examples VALUES (?,?,?)", (item["word"], sentence, article["newsid"]))
-			
+
 			# defined words
 			if item.get("dicid") != None and not item.get("dicid").isspace():
 				# get readings
@@ -101,12 +101,12 @@ if __name__ == '__main__':
 				for index, val in enumerate(item["ruby"]):
 					try: reading += item["ruby"][index]["r"]
 					except KeyError: reading += item["ruby"][index]["s"]
-				
+
 				# find definitions
 				dicid = re.sub("BE-", "", item["dicid"])
 				dicPath = re.sub("json", "dic", files)
-				dicFile = json.load(open(dicPath))
-				
+				dicFile = json.load(open(dicPath, encoding="latin-1"))
+
 				defs = ""
 				for entry in dicFile["reikai"]["entries"][dicid]:
 					# convert ruby-formatted furigana
@@ -117,7 +117,7 @@ if __name__ == '__main__':
 					strippedDef = re.sub("</rt>", "]", strippedDef)
 					defs += strippedDef + "<br>"
 				defs = defs[:-4]
-				
+
 				# find alternate forms
 				altList = []
 				for version in dicFile["reikai"]["entries"][dicid][0]["hyouki"]:
@@ -125,20 +125,20 @@ if __name__ == '__main__':
 					if version != item["base"]:
 						altList.append(version)
 				alt = ", ".join(altList)
-				
+
 				# insert into sql
 				try:
 					c.execute("INSERT INTO Words (word, reading, frequency, alt, definition) VALUES (?,?,?,?,?)", \
 					(item["base"], reading, 1, alt, defs))
 				except sqlite3.IntegrityError:
 					c.execute("UPDATE Words SET frequency = frequency + 1 WHERE word = ?", (item["base"],))
-				
+
 				# add examples
 				for line in articleLines:
-					if item["word"] in line:			
+					if item["word"] in line:
 						sentence = re.sub(item["word"], "<b>"+item["word"]+"</b>", line.strip()+u"。")
 						c.execute("INSERT OR IGNORE INTO Examples VALUES (?,?,?)", (item["base"], sentence, article["newsid"]))
-		
+
 	conn.commit()
 	c.close()
 	conn.close()
